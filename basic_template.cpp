4b636cd535ce07ea6c5b98939ab35c2ef2aab0a1
hasan <s2411176114@ru.ac.bd>
2026-07-16 08:50:46 +0600

    Add basic template

---
+++ basic_template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int INF = 1e18;
+const int mod = 1e9 + 7;
+
+/*Explanation :
+
+
+
+*/
+
+void solve() {
+    
+}
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin>>t;
+
+    while(t--) solve();
+
+    return 0;
+}
